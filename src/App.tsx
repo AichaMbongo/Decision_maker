9a188a08eba34a4f23bfb7d93637bf546353247a
Nathan Nderitu <natenderitu@gmail.com>
2024-04-22 18:48:50 +0300

    feat: add a custom theme to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import theme from './theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
+
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          DECISION MAKING APP
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            DECISION MAKING APP
+          </h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+
+    </ThemeProvider>
+
   );
 }
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme =  createTheme({
+	palette: {
+		primary : {
+			main: '#071e22' ,
+		},
+
+		secondary :{
+			main: '#337357' ,
+
+		},
+
+		background : {
+			default: '#fff', 
+		}
+
+	}
+});
+
+export default theme;
