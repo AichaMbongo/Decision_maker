72472f0eb949d12b217ff89b5afec00499a473c9
Abigail Muthoni Wairi <89918147+Abbymuso1@users.noreply.github.com>
2024-04-24 15:26:49 +0300

    Feat: Added the title section

---
+++ src/components/Title-section.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@fontsource/roboto/500.css';
+import Typography from '@mui/material/Typography';
+
+
+const TitleSection = () => {
+    return (
+        <Typography variant="h6">
+            Title Section
+        </Typography>
+    );
+};
+
+export default TitleSection
