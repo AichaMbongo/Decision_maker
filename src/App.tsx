74e7b15c4bf37f0ff68c5a6b176ceaf33d00b4f4
Aicha Mbongo <zindamoyen2@gmail.com>
2024-04-27 16:35:27 +0300

    refactor: Remove default React logo from App.tsx
    - Removed the default React logo and associated code from the App.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,26 @@ import './App.css';
 import theme from './theme/theme';
 import { ThemeProvider } from '@mui/material/styles';
 import Sidebar from './components/Sidebar';
+import CustomButton from './components/Button';
+import PersonOutlineSharpIcon from '@mui/icons-material/PersonOutlineSharp';
+
 
 
 function App() {
+  const handleClick = () => {
+    console.log("Button clicked!");}
   return (
     <ThemeProvider theme={theme}>
       
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1>
             DECISION MAKING APP
           </h1>
           <h2>Testing</h2>
      
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <CustomButton onClick={handleClick} startIcon={<PersonOutlineSharpIcon />}>
+            Hello</CustomButton>        
         </header>
       </div>
       <div>
