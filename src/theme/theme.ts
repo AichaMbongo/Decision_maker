ebf09515196047f4cbaab7d2d946c3d45aa24fd7
Aicha Mbongo <zindamoyen2@gmail.com>
2024-05-07 19:57:09 +0300

    feat: Add theme for typography

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, withTheme } from '@mui/material/styles';
 
 const theme =  createTheme({
 	palette: {
@@ -9,8 +9,8 @@ const theme =  createTheme({
 		},
 
 		secondary :{
-			main: '#337357' ,
-			// main: '#071e22' ,
+			// main: '#337357' ,
+			main: '#071e22' ,
 
 
 		},
@@ -19,7 +19,39 @@ const theme =  createTheme({
 			default: '#fff', 
 		}
 
-	}
+	},
+	typography: {
+		h1: {
+		  fontSize: 12,
+		  color: '#071e22',
+		  fontWeight: 'bold',
+		},
+
+		h2: {
+			fontSize: 10,
+			color: '#337357',
+			fontWeight: 200,
+		  },
+
+		  subtitle1: {
+			fontSize: 18,
+			color: '#071e22',
+			fontWeight: 500,
+			fontStyle: 'italic',
+		  },
+
+		// body1: {
+		//   fontWeight: 200,
+		//   color: '#071e22',
+		// },
+		button: {
+		  fontStyle: 'italic',
+		  
+		},
+	  },
+
+	
 });
+
 
 export default theme;
