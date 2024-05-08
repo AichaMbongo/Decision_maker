6baab1b9a90d87fd080dced64bba9b1ce5eaf27b
Aicha Mbongo <zindamoyen2@gmail.com>
2024-05-08 07:25:33 +0300

    feat(pages): Create Landing page closes #11

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,13 +1,63 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import { Box, Grid, Typography, Stack } from '@mui/material'
+import Layout from '../components/Layout'
+import GolfCourseIcon from '@mui/icons-material/GolfCourse';
+import CustomButton from '../components/Button';
+import BasicTextField from '../components/input-field';
+import {Field} from '../components/interfaces/InputFieldProps';
+
 //add some temporary links here to enable viewing of pages
-function landing () {
+
+
+const LandingPage: React.FC = () =>  {
+
+const fields: Field[] = [
+  { id: 'filled-basic', label: 'E.g. "To Buy a Car"', variant: 'filled', defaultValue: '' },
+]
+
+const handleClick = () => {
+  console.log("Button is Clicked");
+}
+
   return (
-    <>
-    <HeroSection />
-    </>
+    
+    <Layout >
+      <Box className='hero-section'>
+        <Grid lg={6}>
+        <Box>
+      <Typography variant='h3'>
+Let us Begin With The End in Mind.
+      </Typography>
+      </Box>
+      
+      <Box>
+      <Typography variant='h3'>
+What is Your Goal?
+      </Typography>
+      </Box>
+      <Box> 
+        <GolfCourseIcon 
+        sx={{
+          fontSize: 40
+        }}
+        />
+      </Box>
+      <Box>
+        <BasicTextField fields={fields} />
+      </Box>
+      <Box mt={2}>
+        <CustomButton onClick={handleClick}>
+          PROCEED
+          </CustomButton>
+      </Box>
+      </Grid>
+      </Box>
+
+    </Layout>
+   
   )
 }
 
-export default landing
+export default LandingPage
 
