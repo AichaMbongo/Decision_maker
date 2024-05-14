4c2e0df8d7f4816849b53ebacb38098dc8ac2967
Nathan Nderitu <natenderitu@gmail.com>
2024-05-14 21:35:34 +0300

    fix: delete code in decisionstateprovider file

--- src/components/interfaces/DecisionStateProvider.ts
+++ src/components/interfaces/DecisionStateProvider.ts
@@ -1,9 +1,48 @@
-import React, { createContext, useState, FC, ReactNode, useContext } from 'react';
-import DecisionState from './DecisionState';
-import { DecisionStateContext } from '../../contexts/DecisionStateContext';
+// import React, { createContext, useState, FC, ReactNode, useContext } from 'react';
+// import DecisionState from './DecisionState';
+// import { DecisionStateContext } from '../../contexts/DecisionStateContext';
 
-// const DecisionStateProvider: FC<{ children: ReactNode }> = ({ children }) => {
-//   const [decisionState, setDecisionState] = useState<DecisionState>(defaultDecisionState);
+// // const DecisionStateProvider: FC<{ children: ReactNode }> = ({ children }) => {
+// //   const [decisionState, setDecisionState] = useState<DecisionState>(defaultDecisionState);
+
+// //   const updateDecisionState = (updatedProperties: Partial<DecisionState>) => {
+// //     setDecisionState(prevState => ({
+// //       ...prevState,
+// //       ...updatedProperties,
+// //     }));
+// //   };
+
+// //   return (
+// //     <DecisionStateContext.Provider value={{ decisionState, updateDecisionState }}>
+// //       {children}
+// //     </DecisionStateContext.Provider>
+// //   );
+// // };
+
+// // const useDecisionContext = () => useContext(DecisionStateContext);
+
+// // export { DecisionStateProvider, useDecisionContext };
+
+
+//  interface DecisionStateProviderProps {
+//     children: ReactNode;
+//  }
+//  const defaultDecisionState: DecisionState = {
+//   model: '',
+//   criteria: [],
+//   criteriaComparisons: [],
+//   options: [],
+//   optionComparisons: [],
+// };
+
+// const DecisionStateProvider  = ({ children }: DecisionStateProviderProps) => {
+//   const [decisionState, setDecisionState] = useState<DecisionState>({
+//     model: '',
+//     criteria: [],
+//     criteriaComparisons: [],
+//     options: [],
+//     optionComparisons: [],
+// });
 
 //   const updateDecisionState = (updatedProperties: Partial<DecisionState>) => {
 //     setDecisionState(prevState => ({
@@ -13,50 +52,19 @@ import { DecisionStateContext } from '../../contexts/DecisionStateContext';
 //   };
 
 //   return (
-//     <DecisionStateContext.Provider value={{ decisionState, updateDecisionState }}>
+//     <DecisionStateContext.Provider value={{{ decisionState, setDecisionState }}}>
 //       {children}
 //     </DecisionStateContext.Provider>
+    
 //   );
-// };
+// }
 
-// const useDecisionContext = () => useContext(DecisionStateContext);
+// export default DecisionStateProvider;
 
-// export { DecisionStateProvider, useDecisionContext };
+import React from 'react'
 
+const DecisionStateProvider = {
 
- interface DecisionStateProviderProps {
-    children: ReactNode;
- }
- const defaultDecisionState: DecisionState = {
-  model: '',
-  criteria: [],
-  criteriaComparisons: [],
-  options: [],
-  optionComparisons: [],
 };
 
-const DecisionStateProvider  = ({ children }: DecisionStateProviderProps) => {
-  const [decisionState, setDecisionState] = useState<DecisionState>({
-    model: '',
-    criteria: [],
-    criteriaComparisons: [],
-    options: [],
-    optionComparisons: [],
-});
-
-  const updateDecisionState = (updatedProperties: Partial<DecisionState>) => {
-    setDecisionState(prevState => ({
-      ...prevState,
-      ...updatedProperties,
-    }));
-  };
-
-  return (
-    <DecisionStateContext.Provider value={{{ decisionState, setDecisionState }}}>
-      {children}
-    </DecisionStateContext.Provider>
-    
-  );
-}
-
-export default DecisionStateProvider;
+export default DecisionStateProvider 
