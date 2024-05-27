bddabb4828f92a4118de6983292997d3a3482535
Abigail Muthoni Wairi <89918147+Abbymuso1@users.noreply.github.com>
2024-05-27 16:22:04 +0300

    Feat: Update the New Criteria Page
    Edited the handle submit function to also update the breadcrumbs. Removed onclick to enable the console to read what has been added to the input of the field

--- src/pages/NewCriteria.tsx
+++ src/pages/NewCriteria.tsx
@@ -18,63 +18,67 @@ const NewCriteria = () => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log(formData); // Log the form data in the console
-        navigate('/otherNewCriteria'); // Replace '/newPage' with the path you want to redirect to
+
+        // Call addOtherCriteria to update the breadcrumb navigation
+        addOtherCriteria();
+
+        // Navigate to the new criteria page
+        navigate('/OtherNewCriteria'); // Replace '/newPage' with the path you want to redirect to
     };
 
     const { handleNavigation } = useBreadcrumbs();
     const addOtherCriteria = () => {
-      handleNavigation('/OtherNewCriteria', 'Add Another Criteria');
+        handleNavigation('/OtherNewCriteria', 'Add Another Criteria');
     };
+
     return (
         <Layout>
-          
-         
-                <div style={{ marginLeft: '30px' }}>
-                    <BackButton />
-                </div>
-                 
-                <Stack direction="column" spacing={2} alignItems="center" justifyContent="center" >
-            <Stack className="stack-container">
-                    <Typography variant='h3' align="center">Type in a New Criteria</Typography>
-               
-            
-            <Stack direction="column" spacing={2} alignItems="center" justifyContent="center" style={{ marginBottom: '78px', padding: 3, marginTop: '10px' }}>
-                <FormatListBulletedIcon style={{ fontSize: '56px', padding: '2' }} />
-                <form onSubmit={handleSubmit} style={{alignItems:'center', display:'flex', flexDirection:'column'}}>
-                    <Box
-                        component="div"
-                        sx={{
-                            '& > :not(style)': { m: 1, width: '50ch' },
-                        }}
-                    >
-                        <FormControl variant="standard">
-                            <TextField id="filled-basic"
-                                name="newCriteria"
-                                label="ie. Cost, Comfort"
-                                variant="filled"
-                                value={formData.newCriteria}
-                                onChange={handleChange} />
-                        </FormControl>
-                    </Box>
-                    <Button
-                        variant="contained"
-                        sx={{
-                            borderRadius: '16px',
-                            paddingRight: 2,
-                            paddingLeft: 2,
-                            marginBottom: 2,
-                            minWidth: '200px', // Adjust the width as desired
-                        }}
-                        onClick={addOtherCriteria}
-                        type="submit"
-                        >
-                        Enter New Criteria
-                        </Button>
-                        
 
-                </form>
+            <div style={{ marginLeft: '30px' }}>
+                <BackButton />
+            </div>
+
+            <Stack direction="column" spacing={2} alignItems="center" justifyContent="center" >
+                <Stack className="stack-container">
+                    <Typography variant='h3' align="center">Type in a New Criteria</Typography>
+
+
+                    <Stack direction="column" spacing={2} alignItems="center" justifyContent="center" style={{ marginBottom: '78px', padding: 3, marginTop: '10px' }}>
+                        <FormatListBulletedIcon style={{ fontSize: '56px', padding: '2' }} />
+                        <form onSubmit={handleSubmit} style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+                            <Box
+                                component="div"
+                                sx={{
+                                    '& > :not(style)': { m: 1, width: '50ch' },
+                                }}
+                            >
+                                <FormControl variant="standard">
+                                    <TextField id="filled-basic"
+                                        name="newCriteria"
+                                        label="ie. Cost, Comfort"
+                                        variant="filled"
+                                        value={formData.newCriteria}
+                                        onChange={handleChange} />
+                                </FormControl>
+                            </Box>
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    borderRadius: '16px',
+                                    paddingRight: 2,
+                                    paddingLeft: 2,
+                                    marginBottom: 2,
+                                    minWidth: '200px', // Adjust the width as desired
+                                }}
+                                type="submit"
+                            >
+                                Enter New Criteria
+                            </Button>
+
+
+                        </form>
+                    </Stack>
                 </Stack>
-            </Stack>
             </Stack>
         </Layout>
     );
