ac5a53f8a8bb34616e2eab3c732007066040c905
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-06-03 13:34:38 +0300

    feat: add results page

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,149 +1,187 @@
-
-import { Box, Container,Stack } from '@mui/material';
-import { useContext } from 'react';
-import Typography from '@mui/material/Typography';
+import { Box, Container, Stack } from "@mui/material";
+import { useContext, useEffect } from "react";
+import Typography from "@mui/material/Typography";
 // import { error } from 'console';
-import Button from '@mui/material/Button';
-import Layout from '../components/Layout';
-import AdsClickIcon from '@mui/icons-material/AdsClick';
-import BackButton from '../components/BackButton';
-import { DecisionStateContext } from '../contexts/DecisionStateContext';
-import { useBreadcrumbs } from '../contexts/BreadcrumbsProvider';
-import { NavLink } from 'react-router-dom';
-import CustomButton from '../components/Button';
+import Button from "@mui/material/Button";
+import Layout from "../components/Layout";
+import AdsClickIcon from "@mui/icons-material/AdsClick";
+import BackButton from "../components/BackButton";
+import { DecisionStateContext } from "../contexts/DecisionStateContext";
+import { useBreadcrumbs } from "../contexts/BreadcrumbsProvider";
+import { NavLink } from "react-router-dom";
+import CustomButton from "../components/Button";
+import BarChartComponent from "../components/interfaces/BarChartComponent";
 
-const options = ["Mercedes", "Audi", "BMW"]
-const optionComparisons = []
-
+interface TotalScores {
+  [option: string]: number;
+}
 const ResultsPage = () => {
+  const { decisionState, setDecisionState } = useContext(DecisionStateContext);
+  const totalScores: TotalScores = {};
 
-    const { decisionState, setDecisionState } = useContext(DecisionStateContext);
-    console.log(decisionState)
-    console.log("It didn't work")
+  console.log(decisionState);
+  console.log("Total Scores", totalScores);
+  console.log("Option 1", decisionState.aggregatedPreferences[0]);
+  useEffect(() => {
+    const calculateTotalScores = () => {
+      // Initialize total scores with 0 for each option
+      decisionState.options.forEach((option) => {
+        totalScores[option] = 0;
+      });
 
-    const generatePairwiseComparisons = () =>{
-        const options = decisionState.options
-        console.log("Generating pairwise comparisions")
+      decisionState.criteria.forEach((criterion) => {
+        const criterionAggregated =
+          decisionState.aggregatedPreferences[criterion.name];
+        console.log("Aggregated criteria:", criterionAggregated);
+        console.log("Criterion weight", criterion.weight);
 
-        for (let i = 0; i < options.length -1; i++){            
-            for (let j = i+1; j < options.length; j++) {
-                console.log(options[i], options[j]);
-            }            
-        }
-
-    }
-
-    generatePairwiseComparisons()
-    const { handleNavigation } = useBreadcrumbs();
-    const ViewPreviousDecisions = () => {
-    handleNavigation('/PreviousDecison', 'Previous Decision');
+        // Update total scores with weighted sum
+        decisionState.options.forEach((option) => {
+          totalScores[option] += criterionAggregated[option] * criterion.weight;
+          console.log("Current", totalScores[option]);
+        });
+      });
     };
-    return (
-        
-        <Layout>
-            
-            <Stack>
-                <div style={{ marginLeft: '30px' }}> <BackButton /></div>
-            </Stack>
-            <Container
-                sx={{
-                    display: '100vh',
-                    paddingTop: '2',
-                }}
 
-            >
-                <Box
-                    sx={{
+    calculateTotalScores();
+    console.log(decisionState);
+    console.log("Total scores totality", totalScores);
+  }, [
+    decisionState.criteria,
+    decisionState.aggregatedPreferences,
+    decisionState.options,
+  ]);
 
-                        display: "grid",
-                        height: '80vh',
-                        width: '100%',
+  const { handleNavigation } = useBreadcrumbs();
+  const ViewPreviousDecisions = () => {
+    handleNavigation("/PreviousDecison", "Previous Decision");
+  };
+  return (
+    <Container>
+      <Stack>
+        <div style={{ marginLeft: "30px" }}>
+          {" "}
+          <BackButton />
+        </div>
+      </Stack>
+      <Container
+        sx={{
+          display: "100vh",
+          paddingTop: "2",
+        }}
+      >
+        <Box
+          sx={{
+            display: "grid",
+            height: "80vh",
+            width: "100%",
 
-                        gap: 1,
-                        padding: 2,
-                        boxShadow: 3 ,
-                        backgroundColor: 'white',
+            gap: 1,
+            padding: 2,
+            boxShadow: 3,
+            backgroundColor: "white",
 
-
-                        gridTemplate:
-                            `"result result result result option"
+            gridTemplate: `"result result result result option"
                 "result result result result ranking"
                 "result result result result ranking"
                 ". . save save ranking"
                 `,
-                    }}
-                >
-                    <Box
-                        sx={{
-                            gridArea: "result"
-                            
-                        }}
-                    >
-                        {/* <Typography variant ="h5"></Typography>
-                <Typography></Typography> */}
-
-
-                    </Box>
-                    <Box
-                        sx={{
-                            gridArea: "option",
-                            
-                            borderRadius: 1,
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            boxShadow: 3, // Added shadow effect
-                            backgroundColor: 'white',
-                        }}
-                    >
-                        <Button variant="contained">Add Criteria</Button>  
-                    </Box>
-                    <Box
-                        sx={{
-                            gridArea: "ranking",
-                           
-                            borderRadius: 1,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            gap: 2,
-                            padding: 2.5,
-                            boxShadow: 3, // Added shadow effect
-                            backgroundColor: 'white',
-                        }}
-                    >
-
-                        <Typography variant="body1" alignSelf="flex-start" >Criteria</Typography>
-                        {options.map((option) => (
-                            <Stack gap={3} direction="column" justifyContent="center">
-                                <Stack gap={2} direction="row" alignItems="center" justifyContent="flex-start">
-                                    <AdsClickIcon style={{ fontSize: '56px', padding: '2' }} />
-                                    <Stack>
-                                        <Typography variant='body1'>{option}</Typography>
-                                        <Button variant="contained">Edit Criteria</Button>
-                                    </Stack>
-                                </Stack>
-                            </Stack>
-                        ))}
-                    </Box>
-                    <Box
-                        sx={{
-                            gridArea: "save"
-                        }}>
-
-                        <NavLink to="/PreviousDecision" style={{ textDecoration: 'none', color: 'inherit' }}>
-
-                        <CustomButton onClick={ViewPreviousDecisions}>
-                        Save Decision
-                        </CustomButton>
-                        </NavLink>
-                        
-                    </Box>
-
-
+          }}
+        >
+          <Box
+            sx={{
+              gridArea: "result",
+            }}
+          >
+            <Container sx={{ marginTop: 4 }}>
+              <BackButton />
+              <Typography variant="h4" gutterBottom>
+                Results
+              </Typography>
+              {decisionState.criteria.map((criterion, index) => (
+                <Box key={index} sx={{ marginBottom: 4 }}>
+                  <Typography variant="h6">{criterion.name}</Typography>
+                  <BarChartComponent criterion={criterion.name} />
                 </Box>
+              ))}
+              <Box sx={{ marginTop: 4 }}>
+                <Typography variant="h5" gutterBottom>
+                  Total Scores
+                </Typography>
+                {Object.keys(totalScores).map((option) => (
+                  <Typography key={option} variant="body1">
+                    {`${option}: ${totalScores[option]}`} SHit
+                  </Typography>
+                ))}
+              </Box>
             </Container>
-        </Layout>
-    )
-}
+          </Box>
+          <Box
+            sx={{
+              gridArea: "option",
 
-export default ResultsPage
+              borderRadius: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              boxShadow: 3, // Added shadow effect
+              backgroundColor: "white",
+            }}
+          >
+            <Button variant="contained">Add Criteria</Button>
+          </Box>
+          <Box
+            sx={{
+              gridArea: "ranking",
+
+              borderRadius: 1,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              padding: 2.5,
+              boxShadow: 3, // Added shadow effect
+              backgroundColor: "white",
+            }}
+          >
+            <Typography variant="body1" alignSelf="flex-start">
+              Criteria
+            </Typography>
+            {decisionState.options.map((option) => (
+              <Stack gap={3} direction="column" justifyContent="center">
+                <Stack
+                  gap={2}
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="flex-start"
+                >
+                  <AdsClickIcon style={{ fontSize: "56px", padding: "2" }} />
+                  <Stack>
+                    <Typography variant="body1">{option}</Typography>
+                    <Button variant="contained">Edit Criteria</Button>
+                  </Stack>
+                </Stack>
+              </Stack>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              gridArea: "save",
+            }}
+          >
+            <NavLink
+              to="/PreviousDecision"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <CustomButton onClick={ViewPreviousDecisions}>
+                Save Decision
+              </CustomButton>
+            </NavLink>
+          </Box>
+        </Box>
+      </Container>
+    </Container>
+  );
+  // return <div></div>;
+};
+
+export default ResultsPage;
